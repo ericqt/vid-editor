4a8f3326a8ae1bffcb5bd4b9f61eec7173d6cb52
Eric Tan <etan@theorchard.com>
2020-11-13 19:48:01 -0500

    Implement video player (#3)
    Basic implementation of video player with custom seekbar

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,15 +1,73 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
+import ReactPlayer from 'react-player';
+import SeekBar from './components/seek-bar';
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
+  const [sliderValue, setSliderValue] = useState(0);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const [videoLength, setVideoLength] = useState(0);
+  const [player, setPlayer] = useState(null);
+  const [cutTimes, setCutTimes] = useState([[]]);
+  const [startCut, setCutState] = useState(true);
+
+  const handleProgress = (progressObj) => {
+    setPlayedSeconds(progressObj.playedSeconds);
+    setSliderValue(playedSeconds/videoLength);
+  }
+
+  const handleReady = (vidya) => {
+    setVideoLength(vidya.getDuration());
+    setPlayer(vidya);
+  }
+
+  const setVideoTime = (time) => {
+    setSliderValue(time);
+    player.seekTo(time);
+  }
+
+  const clipsHandler = () => {
+    let cutData = cutTimes;
+    let playerCurrentTime = player.getCurrentTime();
+    console.log('cutTimes data: ', cutTimes);
+    console.log('video current time is: ', playerCurrentTime);
+    switch (startCut){
+      case true:
+        cutData[cutData.length - 1][0] = player.getCurrentTime()
+        setCutState(false);
+        break;
+      default:
+        cutData[cutData.length - 1][1] = player.getCurrentTime()
+        cutData[cutData.length] = [];
+        setCutState(true);
+    }
+    setCutTimes(cutData);
+
+    console.log(cutData);
+  }
 
   return(
     <div>
-      <p>you clicked {count} times biiiiiiitch</p>
-      <button onClick={() => setCount(count+1)}>
-        hello there
-      </button>
+      <div className="videoplayer">
+        <ReactPlayer
+          controls={true}
+          onReady={handleReady}
+          onProgress={handleProgress}
+          url='./assets/videos/test_clip.mp4'
+        />
+        <div style={{padding:'30px'}}>
+          <SeekBar
+            sliderValue={sliderValue}
+            sliderValueSetter={setVideoTime}
+          />
+        </div>
+        <p>the slidervalue is {sliderValue}</p>
+      </div>
+      <div>
+        <button onClick={clipsHandler}>
+          Cut the video
+        </button>
+      </div>
     </div>
   )
 }
