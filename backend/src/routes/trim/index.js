91ee5edf70a97c4228fddf098545a4999008e1bf
erictan <e.qing.tan@gmail.com>
2023-12-29 01:57:19 -0500

    Fixed backend issue

--- backend/src/routes/trim/index.js
+++ backend/src/routes/trim/index.js
@@ -10,16 +10,21 @@ router.route('/')
   .post( (req, res, next) => {
     console.log('ya hit the post in trim endpoint')
     console.log(process.cwd());
-    command = ffmpeg('./public/videos/Serve_3.MP4');
     res.json(req.body);
     cuttimes = req.body['cuttimes'];
     starttime = cuttimes[0][0];
     endtime = cuttimes[0][1] - cuttimes[0][0];
     console.log('attempting to use ffmpeg here');
-    command.inputOptions([`-noaccurate_seek`, `-ss`, 0, `-t`, 10])
-    .outputOptions([`-c:v copy`, `-c:a copy`])
+    command = ffmpeg('./public/videos/Serve_3.MP4')
+      .videoCodec('copy')
+      // ah the audio copy is not working
+      //.audioCodec('copy')
+      .setStartTime(starttime)
+      .setDuration(endtime);
+    console.log('the command is: ', command);
+    command
     .on('error', (err) => {
-      console.log('An Error occurred: ', err.message, err);
+      console.log('An Error occurred: ', err.message, 'and the whole message', err);
     })
     .on('end', () => {
       console.log('processing has finished!');
