35a9d636f88419ac1de48d50e05d6678e30956b2
erictan <e.qing.tan@gmail.com>
2024-01-07 01:42:19 -0500

    promisified ffprobe and tested codec compatibility logic

--- backend/src/routes/trim/index.js
+++ backend/src/routes/trim/index.js
@@ -1,6 +1,44 @@
+const util = require('node:util');
 var router = require('express').Router();
 
 var ffmpeg = require('fluent-ffmpeg');
+const ffprobe = util.promisify(ffmpeg.ffprobe);
+
+const getFirstAudioCodecName = (streams) => {
+  for(const stream of streams){
+    console.log(stream.codec_type)
+    if (stream.codec_type == 'audio'){
+      console.log(stream.codec_name)
+      return stream.codec_name
+    }
+  }
+}
+
+const incompatible_formats = (data) => {
+  format_name = data.format.format_name;
+  audio_codec = getFirstAudioCodecName(data.streams)
+  console.log('format name: ', format_name, 'included?', format_name.includes('mp4'))
+  console.log('audio codec: ', audio_codec)
+  if (format_name.includes('mp4') && audio_codec == 'pcm_s16be'){
+    return true
+  } else {
+    return false
+  }
+}
+
+const prober = async () => {
+  try {
+    data = {}
+    console.log("starting probe")
+    const result = await ffprobe('./public/videos/Serve_3.MP4', (err, data) => data)
+    data = result
+    if(incompatible_formats(data)){
+      console.log('your formats are incompatible.')
+    }
+  } catch (error) {
+    console.log('there was an error', error)
+  }
+}
 
 router.route('/')
   .get( (req, res, next) => {
@@ -21,7 +59,6 @@ router.route('/')
       //.audioCodec('copy')
       .setStartTime(starttime)
       .setDuration(endtime);
-    console.log('the command is: ', command);
     command
     .on('error', (err) => {
       console.log('An Error occurred: ', err.message, 'and the whole message', err);
@@ -31,5 +68,10 @@ router.route('/')
     })
     .save('./public/videos/cut_video.mp4')
   });
+
+router.route('/test')
+  .get( (req, rest, next) => {
+    prober()
+  })
 
 module.exports = router;
