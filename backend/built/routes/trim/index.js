c8b1f23ac8fc88fe70d444fddf635840900f2441
erictan <e.qing.tan@gmail.com>
2024-12-25 01:51:44 -0500

    testing typescript to fix require error message for express

--- backend/built/routes/trim/index.js
+++ backend/built/routes/trim/index.js
@@ -1,21 +1,28 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const util = require('node:util');
 var router = require('express').Router();
 var ffmpeg = require('fluent-ffmpeg');
 var Queue = require('bull');
 import { Queue } from 'bullmq';
-const trimQ = new Queue('trim-q', { redis: { port: 6379, host: 'redis'}});
+const trimQ = new Queue('trim-q', { redis: { port: 6379, host: 'redis' } });
 // const ffprobe = util.promisify(ffmpeg.ffprobe);
-
 const getFirstAudioCodecName = (streams) => {
-  for(const stream of streams){
-    console.log(stream.codec_type)
-    if (stream.codec_type == 'audio'){
-      console.log(stream.codec_name)
-      return stream.codec_name
+    for (const stream of streams) {
+        console.log(stream.codec_type);
+        if (stream.codec_type == 'audio') {
+            console.log(stream.codec_name);
+            return stream.codec_name;
+        }
     }
-  }
-}
-
+};
 /*
 const incompatible_formats = (data) => {
   format_name = data.format.format_name;
@@ -50,26 +57,24 @@ router.route('/test')
   })
 
 */
-
 const cuttimesFormatter = (item, index, formatted_times) => {
-  formatted_times.push([item[0], item[1]-item[0]])
-}
-
+    formatted_times.push([item[0], item[1] - item[0]]);
+};
 router.route('/')
-  .get( async (req, res, next) => {
-    console.log('ya hit the get in trim endpoint')
+    .get((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log('ya hit the get in trim endpoint');
     res.json("booyakasha");
-  })
-  .post( async (req, res, next) => {
+}))
+    .post((req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(process.cwd());
     res.json(req.body);
     command = ffmpeg('./videos/test_vid.mkv');
-    formatted_times = []
+    formatted_times = [];
     cuttimes = req.body['cuttimes'];
-    cuttimes.forEach(cuttimesFormatter())
+    cuttimes.forEach(cuttimesFormatter());
     starttime = cuttimes[0][0];
     endtime = cuttimes[0][1] - cuttimes[0][0];
-    console.log('the formatted cut times are: ', formatted_times)
+    console.log('the formatted cut times are: ', formatted_times);
     console.log('attempting to use ffmpeg here');
     // command = ffmpeg('./public/videos/Serve_3.MP4')
     //   .videoCodec('copy')
@@ -90,7 +95,7 @@ router.route('/')
     //   'starttime': starttime,
     //   'endtime': endtime
     // });
-    console.log('job has been added')
+    console.log('job has been added');
     //res.json("the job is done")
     //result = await command.inputOptions([`-noaccurate_seek`, `-ss`, starttime])
     //.outputOptions([`-to`, endtime, `-c:v copy`, `-c:a copy`])
@@ -102,15 +107,13 @@ router.route('/')
     //})
     //.save('./videos/cut_video.mp4')
     //res.json('this is the result');
-  });
-
+}));
 // Move this function in to it's own file and create a new
 // docker service that starts the script with `node`
-trimQ.process( async(job, done) => {
-  done();
-  console.log(job.data);
-  console.log('did some work here');
-  return true;
-});
-
+trimQ.process((job, done) => __awaiter(void 0, void 0, void 0, function* () {
+    done();
+    console.log(job.data);
+    console.log('did some work here');
+    return true;
+}));
 module.exports = router;
