a957cfb7ebf96ea708e863b8d7297765dc484296
erictan <e.qing.tan@gmail.com>
2025-05-13 18:44:15 -0400

    finished bullmq implementation

--- backend/src/consumer.js
+++ backend/src/consumer.js
@@ -6,48 +6,32 @@ import ffmpeg from 'fluent-ffmpeg';
 const consumer = new worker.Worker(
     'trim-q', 
     async job => {
-        console.log('working now');
-        if (job.name == 'trim-q') {
-            console.log('starting work now with data:', job.data);
-            let starttime = job.data['starttime']
-            let endtime = job.data['endtime']
-            let command = ffmpeg('./public/videos/Serve_3.MP4')
-               //.videoCodec('copy')
-               // ah the audio copy is not working
-               //.audioCodec('copy')
-               //.setStartTime(starttime)
-               //.setDuration(endtime);
-            //const result = await command
-            //.on('error', (err) => {
-            //  console.log('An Error occurred in the first: ', err.message, 'and the whole message', err);
-            //})
-            //.on('end', () => {
-            //  console.log('processing has finished!');
-            //})
-            //.save('./public/videos/cut_video.mp4')
+        ffmpeg.setFfmpegPath(process.env.FFMPEG_PATH);
+        console.log('starting work now with data:', job.data);
+        const starttime = job.data['start']
+        const endtime = job.data['end']
+        const index = job.data['index']
+        let command = ffmpeg('./public/videos/Serve_3.MP4')
 
-
-            console.log('starting the ffmpeg command')
-            try{
-                ffmpeg.setFfmpegPath('/var/app/ffmpeg/bin/ffmpeg');
-                const result = await command.inputOptions(['-noaccurate_seek', '-ss', Math.round(starttime)])
-                .outputOptions(['-t', Math.round(endtime), '-c', 'copy'])
-                .on('start', (commandLine) => {
-                    console.log('spawned ffmpeg commmand as:', commandLine)
-                })
-                .on('error', (err) => {
-                  console.log('An Error occurred in the second: ', err.message, 'and the whole message', err);
-                })
-                .on('end', () => {
-                  console.log('processing has finished!');
-                })
-                //.save('./videos/cut_video.mp4')
-                .save('./public/videos/cut_video.mp4')
-            } catch (err) {
-                console.log('caught an error here: ', err);
-            }
-            console.log(result)
+        console.log('starting the ffmpeg command')
+        try{
+            const result = await command.inputOptions(['-noaccurate_seek', '-ss', Math.round(starttime)])
+            .outputOptions(['-t', Math.round(endtime), '-c', 'copy'])
+            .on('start', (commandLine) => {
+                console.log('spawned ffmpeg commmand as:', commandLine)
+            })
+            .on('error', (err) => {
+              console.log('An Error occurred in the second: ', err.message, 'and the whole message', err);
+            })
+            .on('end', () => {
+              console.log('processing has finished!');
+            })
+            //.save('./videos/cut_video.mp4')
+            .save('./public/videos/cut_video_'+index+'.mp4')
+        } catch (err) {
+            console.log('caught an error here: ', err);
         }
+        console.log(result)
     }, 
     {
         connection: {
--- backend/src/routes/trim/index.js
+++ backend/src/routes/trim/index.js
@@ -1,64 +1,32 @@
 import { Router } from 'express'
 const router = Router();
 
-import ffmpeg from 'fluent-ffmpeg';
 import { Queue } from 'bullmq'
 const trimQ = new Queue('trim-q', { connection: { port: 6379, host: 'redis'}});
-// const util = require('node:util');
-// const ffprobe = util.promisify(ffmpeg.ffprobe);
 
-const getFirstAudioCodecName = (streams) => {
-  for(const stream of streams){
-    console.log(stream.codec_type)
-    if (stream.codec_type == 'audio'){
-      console.log(stream.codec_name)
-      return stream.codec_name
-    }
-  }
+const formatJobsPayload = (rawData) => {
+    return rawData.map( (times, index) => ({
+      'name': 'lawl',
+      data: {
+        index,
+        start: times[0].toFixed(2),
+        end  : (times[1] - times[0]).toFixed(2)
+      }
+    }));
 }
 
-/*
-const incompatible_formats = (data) => {
-  format_name = data.format.format_name;
-  console.log(data)
-  audio_codec = getFirstAudioCodecName(data.streams)
-  console.log('format name: ', format_name, 'included?', format_name.includes('mp4'))
-  console.log('audio codec: ', audio_codec)
-  if (format_name.includes('mp4') && audio_codec == 'pcm_s16be'){
-    return true
-  } else {
-    return false
-  }
-}
-
-const prober = async () => {
-  try {
-    data = {}
-    console.log("starting probe")
-    const result = await ffprobe('./public/videos/Serve_3.MP4', (err, data) => data)
-    data = result
-    if(incompatible_formats(data)){
-      console.log('your formats are incompatible.')
-    }
-  } catch (error) {
-    console.log('there was an error', error)
-  }
-}
-
-*/
 router.route('/test')
   .get( (req, res, next) => {
     //prober()
     console.log('you hit the test endpoint');
-    trimQ.add('trim-q', {
-      'starttime': 'test'
-    });
+    let name = 'lawl'
+    trimQ.addBulk('trim-q', [
+      { name, data: { paint: 'car' } },
+      { name, data: { paint: 'house' } },
+      { name, data: { paint: 'boat' } },
+    ]);
     res.status(200).send('bar');
   })
-
-const cuttimesFormatter = (item, index, formatted_times) => {
-  formatted_times.push([item[0], item[1]-item[0]])
-}
 
 router.route('/')
   .get( async (req, res, next) => {
@@ -67,20 +35,13 @@ router.route('/')
   })
   .post( async (req, res, next) => {
     console.log('in trim index post method', req.body);
-    console.log(process.cwd());
     res.json(req.body);
-    let command = ffmpeg('./videos/test_vid.mkv');
     let formatted_times = []
     let cuttimes = req.body['cuttimes'];
-    //cuttimes.forEach(cuttimesFormatter())
-    let starttime = cuttimes[0][0];
-    let endtime = cuttimes[0][1] - cuttimes[0][0];
-    //console.log('the formatted cut times are: ', formatted_times)
+    const bulkedJobs = formatJobsPayload(cuttimes);
+    console.log(bulkedJobs)
     console.log('attempting to use ffmpeg here');
-    const job = trimQ.add('trim-q', {
-      'starttime': starttime,
-      'endtime': endtime
-    });
+    const jobs = await trimQ.addBulk(bulkedJobs);
     console.log('job has been added')
   });
 
